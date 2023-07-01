b40ad5bcb41ddc9553781a96fcf405d41fc2bac2
Baran Yancı <73401941+y4nci@users.noreply.github.com>
2023-07-01 07:41:27 +0300

    export constants

--- src/main/constants.ts
+++ src/main/constants.ts
@@ -1,17 +1,17 @@
-const TRANSFERMARKT_URL = 'https://transfermarkt.com';
+export const TRANSFERMARKT_URL = 'https://transfermarkt.com';
 
-const PREMIER_LEAGUE_URL = 'https://www.transfermarkt.com/premier-league/startseite/wettbewerb/GB1';
-const LA_LIGA_URL = 'https://www.transfermarkt.com/laliga/startseite/wettbewerb/ES1';
-const SERIE_A_URL = 'https://www.transfermarkt.com/serie-a/startseite/wettbewerb/IT1';
-const BUNDESLIGA_URL = 'https://www.transfermarkt.com/bundesliga/startseite/wettbewerb/L1';
-const LIGUE_1_URL = 'https://www.transfermarkt.com/ligue-1/startseite/wettbewerb/FR1';
-const SUPER_LIG_URL = 'https://www.transfermarkt.com/super-lig/startseite/wettbewerb/TR1';
-const EREDIVISIE_URL = 'https://www.transfermarkt.com/eredivisie/startseite/wettbewerb/NL1';
-const PRIMEIRA_LIGA_URL = 'https://www.transfermarkt.com/liga-nos/startseite/wettbewerb/PO1';
-const SCOTTISH_PREMIERSHIP_URL = 'https://www.transfermarkt.com/scottish-premiership/startseite/wettbewerb/SC1';
-const JUPILER_PRO_LEAGUE_URL = 'https://www.transfermarkt.com/jupiler-pro-league/startseite/wettbewerb/BE1';
+export const PREMIER_LEAGUE_URL = 'https://www.transfermarkt.com/premier-league/startseite/wettbewerb/GB1';
+export const LA_LIGA_URL = 'https://www.transfermarkt.com/laliga/startseite/wettbewerb/ES1';
+export const SERIE_A_URL = 'https://www.transfermarkt.com/serie-a/startseite/wettbewerb/IT1';
+export const BUNDESLIGA_URL = 'https://www.transfermarkt.com/bundesliga/startseite/wettbewerb/L1';
+export const LIGUE_1_URL = 'https://www.transfermarkt.com/ligue-1/startseite/wettbewerb/FR1';
+export const SUPER_LIG_URL = 'https://www.transfermarkt.com/super-lig/startseite/wettbewerb/TR1';
+export const EREDIVISIE_URL = 'https://www.transfermarkt.com/eredivisie/startseite/wettbewerb/NL1';
+export const PRIMEIRA_LIGA_URL = 'https://www.transfermarkt.com/liga-nos/startseite/wettbewerb/PO1';
+export const SCOTTISH_PREMIERSHIP_URL = 'https://www.transfermarkt.com/scottish-premiership/startseite/wettbewerb/SC1';
+export const JUPILER_PRO_LEAGUE_URL = 'https://www.transfermarkt.com/jupiler-pro-league/startseite/wettbewerb/BE1';
 
-const LEAGUE_URLS: Record<LeagueTitle, string> = {
+export const LEAGUE_URLS: Record<LeagueTitle, string> = {
     'Premier League': PREMIER_LEAGUE_URL,
     'La Liga': LA_LIGA_URL,
     'Serie A': SERIE_A_URL,
