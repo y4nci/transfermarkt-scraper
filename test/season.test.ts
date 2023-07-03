f4050b5a2ce9a8c5c67b447d6c4b8a4ed26edff8
Baran Yancı <73401941+y4nci@users.noreply.github.com>
2023-07-03 15:05:00 +0300

    improve test cases for season

--- test/season.test.ts
+++ test/season.test.ts
@@ -8,13 +8,47 @@ describe('Season', () => {
     });
 
     it('should initialize a season', async () => {
+        const expected = [
+            'https://transfermarkt.com/as-monaco/spielplan/verein/162/saison_id/2016',
+            'https://transfermarkt.com/fc-paris-saint-germain/spielplan/verein/583/saison_id/2016',
+            'https://transfermarkt.com/ogc-nizza/spielplan/verein/417/saison_id/2016',
+            'https://transfermarkt.com/olympique-lyon/spielplan/verein/1041/saison_id/2016',
+            'https://transfermarkt.com/olympique-marseille/spielplan/verein/244/saison_id/2016',
+            'https://transfermarkt.com/fc-girondins-bordeaux/spielplan/verein/40/saison_id/2016',
+            'https://transfermarkt.com/fc-nantes/spielplan/verein/995/saison_id/2016',
+            'https://transfermarkt.com/as-saint-etienne/spielplan/verein/618/saison_id/2016',
+            'https://transfermarkt.com/fc-stade-rennes/spielplan/verein/273/saison_id/2016',
+            'https://transfermarkt.com/ea-guingamp/spielplan/verein/855/saison_id/2016',
+            'https://transfermarkt.com/losc-lille/spielplan/verein/1082/saison_id/2016',
+            'https://transfermarkt.com/sco-angers/spielplan/verein/1420/saison_id/2016',
+            'https://transfermarkt.com/fc-toulouse/spielplan/verein/415/saison_id/2016',
+            'https://transfermarkt.com/fc-metz/spielplan/verein/347/saison_id/2016',
+            'https://transfermarkt.com/montpellier-hsc/spielplan/verein/969/saison_id/2016',
+            'https://transfermarkt.com/dijon-fco/spielplan/verein/2969/saison_id/2016',
+            'https://transfermarkt.com/sm-caen/spielplan/verein/1162/saison_id/2016',
+            'https://transfermarkt.com/fc-lorient/spielplan/verein/1158/saison_id/2016',
+            'https://transfermarkt.com/as-nancy-lorraine/spielplan/verein/1159/saison_id/2016',
+            'https://transfermarkt.com/sc-bastia/spielplan/verein/595/saison_id/2016'
+        ];
         await season.init();
-        expect(season.getTeamURLs().length).toBeGreaterThan(0);
-        console.log(season.getTeamURLs());
+
+        expect(season.getTeamURLs()).toEqual(expected);
     });
 
     it('should fetch teams', async () => {
+        const expected = [
+            'AS Monaco',           'Paris Saint-Germain',
+            'OGC Nice',            'Olympique Lyon',
+            'Olympique Marseille', 'FC Girondins Bordeaux',
+            'FC Nantes',           'AS Saint-Étienne',
+            'Stade Rennais FC',    'EA Guingamp',
+            'LOSC Lille',          'Angers SCO',
+            'FC Toulouse',         'FC Metz',
+            'Montpellier HSC',     'Dijon FCO',
+            'SM Caen',             'FC Lorient',
+            'AS Nancy-Lorraine',   'SC Bastia'
+        ];
         await season.fetchTeams();
-        expect(season.getTeams().length).toBeGreaterThan(0);
-    });
+        expect(season.getTeams().map(team => team.getName())).toEqual(expected);
+    }, 8_000_000);
 });
