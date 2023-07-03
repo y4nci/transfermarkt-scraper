37ec83270d5dcb2a84212c17cd38ed01b38d1089
Baran Yancı <73401941+y4nci@users.noreply.github.com>
2023-07-03 23:01:34 +0300

    edit main test module

--- test/main.test.ts
+++ test/main.test.ts
@@ -2,14 +2,42 @@ import { fetchLeagueSeason, getTeamURLsForLastNSeasons } from '../dist';
 
 describe('main', () => {
     it('should fetch a league season', async () => {
+        const expected = [
+            'https://transfermarkt.com/fc-bayern-munchen/startseite/verein/27/saison_id/2020',
+            'https://transfermarkt.com/borussia-dortmund/startseite/verein/16/saison_id/2020',
+            'https://transfermarkt.com/rasenballsport-leipzig/startseite/verein/23826/saison_id/2020',
+            'https://transfermarkt.com/bayer-04-leverkusen/startseite/verein/15/saison_id/2020',
+            'https://transfermarkt.com/borussia-monchengladbach/startseite/verein/18/saison_id/2020',
+            'https://transfermarkt.com/eintracht-frankfurt/startseite/verein/24/saison_id/2020',
+            'https://transfermarkt.com/vfl-wolfsburg/startseite/verein/82/saison_id/2020',
+            'https://transfermarkt.com/hertha-bsc/startseite/verein/44/saison_id/2020',
+            'https://transfermarkt.com/tsg-1899-hoffenheim/startseite/verein/533/saison_id/2020',
+            'https://transfermarkt.com/vfb-stuttgart/startseite/verein/79/saison_id/2020',
+            'https://transfermarkt.com/1-fsv-mainz-05/startseite/verein/39/saison_id/2020',
+            'https://transfermarkt.com/sc-freiburg/startseite/verein/60/saison_id/2020',
+            'https://transfermarkt.com/fc-schalke-04/startseite/verein/33/saison_id/2020',
+            'https://transfermarkt.com/1-fc-koln/startseite/verein/3/saison_id/2020',
+            'https://transfermarkt.com/sv-werder-bremen/startseite/verein/86/saison_id/2020',
+            'https://transfermarkt.com/fc-augsburg/startseite/verein/167/saison_id/2020',
+            'https://transfermarkt.com/1-fc-union-berlin/startseite/verein/89/saison_id/2020',
+            'https://transfermarkt.com/arminia-bielefeld/startseite/verein/10/saison_id/2020'
+        ];
+        let equal = true;
         const leagueSeason = await fetchLeagueSeason('BUNDESLIGA', 2020);
-        console.log(leagueSeason);
-        expect(leagueSeason).toBeDefined();
+        
+        for (let i = 0; i < expected.length; i++) {
+            if (leagueSeason.getTeamURLs()[i] !== expected[i]) {
+                equal = false;
+                break;
+            }
+        }
+
+        expect(equal).toBe(true);
     });
 
     it('should fetch team URLs for last n seasons', async () => {
-        const teamURLs = await getTeamURLsForLastNSeasons('BUNDESLIGA', 3);
+        const teamURLs = await getTeamURLsForLastNSeasons('BUNDESLIGA', 5, true);
         console.log(teamURLs);
         expect(teamURLs).toBeDefined();
-    });
+    }, 20_000);
 });
